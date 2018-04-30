e3320dcc5e2af3b1d1a1f8baf4e825f884cd6017
unknown <Joseph.Kampa@gmail.com>
2018-04-30 18:11:22 -0500

    Modal Work, WeatherAPI Setting Change JS

--- assets/javascript/weatherapi.js
+++ assets/javascript/weatherapi.js
@@ -11,7 +11,6 @@ $.ajax({
         console.log(queryURL);
         console.log(weatherAPIResponse);
 
-        //$(".city").html("<h1>" + response.name + " Weather Details</h1>");
         $("#weatherAPITemp").text(weatherAPIResponse.main.temp);
         $("#weatherAPICondition").text(weatherAPIResponse.weather[0].main);
         $("#weatherAPIIcon").html('<img src="http://openweathermap.org/img/w/' + weatherAPIResponse.weather[0].icon + '.png">');
