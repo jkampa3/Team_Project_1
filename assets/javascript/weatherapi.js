44ed718a8543e0be1a2e4e9c3afa06e7c432ae89
unknown <Joseph.Kampa@gmail.com>
2018-05-03 16:42:27 -0500

    Added WeatherAPI to Search OnClick

--- assets/javascript/weatherapi.js
+++ assets/javascript/weatherapi.js
@@ -1,18 +1,22 @@
-var apiKey = "33a8ac12edb4b6b08d1eec131d0e4795";
-var city = "minneapolis";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
+function weatherAPICall() {
 
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-    .then(function (weatherAPIResponse) {
-        
-        console.log(queryURL);
-        console.log(weatherAPIResponse);
+    var apiKey = "33a8ac12edb4b6b08d1eec131d0e4795";
+    var city = $("#formCityZipInput").val();
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
 
-        $("#weatherAPITemp").text(weatherAPIResponse.main.temp);
-        $("#weatherAPICondition").text(weatherAPIResponse.weather[0].main);
-        $("#weatherAPIIcon").html('<img src="http://openweathermap.org/img/w/' + weatherAPIResponse.weather[0].icon + '.png">');
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (weatherAPIResponse) {
 
-    });
+            //console.log(queryURL);
+            //console.log(weatherAPIResponse);
+
+            $("#weatherAPITemp").text(weatherAPIResponse.main.temp);
+            $("#weatherAPICondition").text(weatherAPIResponse.weather[0].main);
+            $("#weatherAPIIcon").html('<img src="http://openweathermap.org/img/w/' + weatherAPIResponse.weather[0].icon + '.png">');
+
+        });
+
+};
